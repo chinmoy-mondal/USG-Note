88d6cc3c9e745d2013171d12b40870873868b05a
Chinmoy Mondal <chinmoymondal791@gmail.com>
2025-10-16 22:23:23 +0600

    splash logo is shown but size is not right

--- app/src/main/java/com/purnota/usgnote/MainActivity.kt
+++ app/src/main/java/com/purnota/usgnote/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.purnota.usgnote
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install splash screen
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Optional: If you want a short delay, you can keep it here
+        // Otherwise, the splash screen disappears instantly
     }
 }
